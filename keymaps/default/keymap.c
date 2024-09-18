3a697c225621fa858f2d8f641ae1eb4256ff8c86
atomofiron <atomofiron@gmail.com>
2024-09-19 00:22:33 +0200

    swap dot and comma back

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -12,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_LBRC,
                  KC_EQL,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_RBRC,
         KC_TAB,  KC_MINS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_RGHT, KC_BSPC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, FN_1,
-                 KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LEFT, KC_UP,   KC_N,    KC_M,    KC_DOT,  KC_COMM, KC_SLSH, KC_BSLS,
+                 KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LEFT, KC_UP,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
                                    UG_NEXT, KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  KC_ENT,  KC_DOWN, KC_RSFT, KC_TAB,  KC_MUTE
     ),
     [LAYER_1] = LAYOUT(
